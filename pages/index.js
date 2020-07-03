85edde92a50a21a49381ab5fadecfd0913a3b469
lugrao <elugrao@gmail.com>
2020-07-03 13:01:57 -0300

    crea server con express, conecta socket entre server y cliente

---
+++ pages/index.js
@@ -0,0 +1,58 @@
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
+
+const socket = io();
+
+export default function App(msjs) {
+  const [mensajes, setMensajes] = useState([{}]);
+  const [nuevoMensaje, setNuevoMensaje] = useState({ contenido: "" });
+  const [enviado, setEnviado] = useState(false);
+  const [refresh, setRefresh] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/mensajes").then(async (res) => {
+      const msjs = await res.json();
+      setMensajes(msjs);
+    });
+
+    if (enviado) {
+      socket.emit("mensaje-del-cliente", nuevoMensaje);
+      setNuevoMensaje({ contenido: "" });
+      setEnviado(false);
+    }
+  });
+
+  function enviar() {
+    event.preventDefault();
+    setEnviado(true);
+  }
+  function capturarValor(event) {
+    setNuevoMensaje({ contenido: event.target.value });
+  }
+  function recargar() {
+    setRefresh(!refresh);
+  }
+
+  return (
+    <div>
+      <h1>chat</h1>
+
+      <form onSubmit={enviar}>
+        {mensajes.map((mensaje, index) => {
+          return <p key={index}>{mensaje.contenido}</p>;
+        })}
+
+        <input onChange={capturarValor} value={nuevoMensaje.contenido}></input>
+        <button type="submit" onClick={enviar}>
+          enviar
+        </button>
+      </form>
+    </div>
+  );
+}
+
+App.getInitialProps = async (ctx) => {
+  const res = await fetch("http://localhost:3000/mensajes");
+  const json = await res.json();
+  return { msjs: json };
+};
--- server.js
+++ server.js
@@ -1,2 +1,36 @@
 const app = require("express")();
 const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const nextHandler = nextApp.getRequestHandler();
+
+let port = 3000;
+
+let mensajes = [{contenido: "empezá a chatear"}];
+
+io.on("connect", (socket) => {
+
+  socket.emit("mensaje-del-servidor", mensajes);
+
+  socket.on("mensaje-del-cliente", (msj) => {
+    mensajes.push(msj);
+  });
+});
+
+nextApp.prepare().then(() => {
+  app.get("/mensajes", (req, res) => {
+    res.json(mensajes);
+  });
+
+  app.get("*", (req, res) => {
+    return nextHandler(req, res);
+  });
+});
+
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`listo el pollo en puerto http://localhost:${port}`);
+});
